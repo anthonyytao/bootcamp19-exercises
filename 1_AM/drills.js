1943325c1c3c4bff2208eb85bb599bfd5d10e71e
anthonyytao <anthonytao@college.harvard.edu>
2019-01-07 12:53:57 -0500

    1 AM solutions

--- 1_AM/drills.js
+++ 1_AM/drills.js
@@ -4,24 +4,66 @@ const assert = require("assert");
 
 const tokenize = str => {
   // TODO - write a function which converts a multi-word string into an array of words
+  const arr = str.split(' ');
+  console.log(arr);
 };
+
+tokenize("hello my name is anthony");
 
 const reverse = str => {
   // TODO - write a function which reverses the string
+  let reverse = "";
+  for (let i = str.length - 1; i>=0; i--) {
+    reverse += str[i];
+  }
+  console.log(reverse);
+  return reverse;
 };
+
+reverse("abc");
 
 const uniqueOnes = arr => {
   // TODO - write a function which returns the inputted array without duplicate elements
+  const findUnique = function(item, pos, self) {
+    return self.indexOf(item) === pos;
+  }
+  const uniqueArray = arr.filter(findUnique);
+  console.log(uniqueArray);
+  return uniqueArray;
 };
+
+const a = [1,2,3,4,1,2];
+uniqueOnes(a);
 
 const factorial = num => {
   // TODO - write a function which returns the factorial of a positive integer
+  if (num === 0) {
+    return 1;
+  }
+  else {
+    return(num * factorial(num-1));
+  }
 };
+
+console.log(factorial(3));
 
 const zip = (arr1, arr2) => {
   // TODO - write a function which combines two arrays into an array of 2-element arrays and returns -1
   // if the two arrays are of unequal length
   // Example: zip([1, 2, 3], [5, 3, 1]) === [[1, 5], [2, 3], [3, 1]]
+  if (arr1.length !== arr2.length) {
+    return -1;
+  }
+  else {
+    finalArray = [];
+    for (let i = 0; i < arr1.length; i++) {
+      newArray = [];
+      newArray.push(arr1[i]);
+      newArray.push(arr2[i]);
+      finalArray.push(newArray);
+    }
+    return finalArray;
+  }
 };
 
 const unzip = arr => {
@@ -43,4 +85,5 @@ const announceDate = () => {
 assert(1 < 2);
 assert(1 + 2 === 3);
 assert([2, 3][0] === 2);
-// asssert (reverse("3df") === "fd3")
+assert(reverse("3df") === "fd3");
+assert(factorial(3) === 6);

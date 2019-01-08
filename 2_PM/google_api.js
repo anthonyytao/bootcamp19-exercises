21932bc62fe4c83f11b0bdbd9e65122fdb88feea
anthonyytao <anthonytao@college.harvard.edu>
2019-01-08 16:27:22 -0500

    2 PM solutions

--- 2_PM/google_api.js
+++ 2_PM/google_api.js
@@ -1,1 +1,8 @@
 // TODO - create client object for Google API connection which can be exported
+const config = require('./config.js');
+
+const googleMapsClient = require('@google/maps').createClient({
+    key:config.key,
+  });
+
+module.exports = googleMapsClient
--- 2_PM/placeinfo.js
+++ 2_PM/placeinfo.js
@@ -2,3 +2,12 @@
 / returns the following string, formatted with the appropriate values:
 
 'INPUTTED_PLACE is located at coordinates RETURNED_COORDINATES, at the address RETURNED_ADDRESS.' */
+const googleMapsClient = require('./google_api.js');
+
+googleMapsClient.geocode({
+    address: process.argv[2]
+  }, function(err, response) {
+    if (!err) {
+      console.log(`${process.argv[2]} is located at coordinates (${response.json.results[0]['geometry']['location']['lat']},${response.json.results[0]['geometry']['location']['lng']}), at the address ${response.json.results[0]['formatted_address']}.`);
+    }
+  });
